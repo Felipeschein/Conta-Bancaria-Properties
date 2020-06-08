79d956bc3a70d2a7a0855554f92f84a08e5deff9
Felipe Schein <felipe_str@hotmail.com>
2020-06-08 15:11:46 -0300

    cadastro de conta bancaria

---
+++ ExercicioContaBancaria/Conta.cs
@@ -0,0 +1,65 @@
+﻿
+using System.Globalization;
+
+
+namespace ExercicioContaBancaria
+{
+    class Conta
+    {
+        private string _nome;
+        private int _numeroConta; 
+        public double Saldo { get; private set; }
+        
+
+
+        public Conta() {
+        }
+
+        public Conta(string nome, int numeroConta)
+        {
+            _nome = nome;
+            _numeroConta = numeroConta;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+        public int NumerConta
+        {
+            get { return _numeroConta; }
+            set
+            {
+                if (_numeroConta == 0)
+                {
+                    _numeroConta = value;
+                }
+            }
+        }
+
+        
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo = (Saldo - valor) - 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + _numeroConta + ", "
+                + "Titular: " + _nome + ", "
+                + "Saldo: $ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ExercicioContaBancaria/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c = new Conta();
+
+            Console.Write("Entre com o número da conta: ");
+            c.NumerConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            c.Nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char op = char.Parse(Console.ReadLine());
+
+            if (op == 's' || op == 'S')
+            {
+                Console.Write("Entre o valor do depósito inicial: ");
+                double depositoIncial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c.Deposito(depositoIncial);
+            }
+
+            
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(c);
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Deposito(deposito);
+            Console.WriteLine();
+            
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(c);
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Saque(saque);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(c);
+
+
+        }
+    }
+}
